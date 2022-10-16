ece193b469745f543c19395649bafc2637d5b8ee
DuckCoder1101 <crisfavaedu@gmail.com>
2022-10-16 15:01:12 -0300

    Primeira versão do jogo

---
+++ utils/types.ts
@@ -0,0 +1,4 @@
+export interface wordmodel {
+    brand: string;
+    tips: Array<string>;
+}
---
+++ utils/words.ts
@@ -0,0 +1,36 @@
+import type { wordmodel } from "./types";
+
+const words: Array<wordmodel> = [
+  {
+    brand: "null",
+    tips: [
+
+    ]
+  },
+  {
+    brand: "apple",
+    tips: [
+      "Marca americana.",
+      "Fabrica celulares, computadores, e outros.",
+      "Conhecida por sua logo da maçã mordida.",
+    ],
+  },
+  {
+    brand: "samsung",
+    tips: [
+      "Marca coreana.",
+      "Fabrica celulares, notebooks e televisões.",
+      "Conhecida por seus celulares tirarem as melhores fotos da lua.",
+    ],
+  },
+  {
+    brand: "nestle",
+    tips: [
+      "Marca brasileira.",
+      "Fabrica muitos tipos de alimentos, incluindo cereais, chocolates e outros.",
+      "Conhecida por suas deliciosas caixas de chocolates.",
+    ],
+  },
+];
+
+export default words
