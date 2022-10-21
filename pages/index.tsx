564711c1e7967a19c2d024899a2884709c64e9fa
DuckCoder1101 <crisfavaedu@gmail.com>
2022-10-21 16:45:00 -0300

    Backup 2

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,10 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import { useEffect, useState } from 'react'
-import styles from '../styles/Home.module.css'
-import words from '../utils/words'
+import type { NextPage } from "next";
+import Head from "next/head";
+import { useEffect, useState } from "react";
+import styles from "../styles/Home.module.css";
+import words from "../utils/words";
 
 const Home: NextPage = () => {
-
   const [correctWord, setWord] = useState(words[0]);
   const [points, setPoints] = useState(0);
   const [currentTip, setTip] = useState(0);
@@ -13,15 +12,30 @@ const Home: NextPage = () => {
   // Set the first random word
   useEffect(() => {
     if (correctWord.brand == "null") {
-      setWord(words[Math.floor(Math.random() * (words.length -1) +1)]);
+      setWord(words[Math.floor(Math.random() * (words.length - 1) + 1)]);
     }
-  }, [correctWord]);
+  }, [correctWord.brand]);
+
+  useEffect(() => {
+    let input = document.getElementById("randomBrand") as HTMLInputElement;
+
+    setInterval(async () => {
+      let { brand } = await words[Math.floor(Math.random() * words.length)];
+      input.value = "";
+
+      if (brand == "null") brand = "Guess the Brand";
+
+      for (let i = 0; i < brand.length; i++) {
+        input.value += brand[i];
+        await new Promise((resolve) => setTimeout(resolve, 150));
+      }
+    }, 4500);
+  }, []);
 
   const nextLevel = () => {
-
     // Clear the tips and get another word
     setTip(0);
-    setWord(words[Math.floor(Math.random() * (words.length -1) +1)]);
+    setWord(words[Math.floor(Math.random() * (words.length - 1) + 1)]);
 
     // Loop on all tips
     for (let li of Array.from(document.querySelectorAll(".tip"))) {
@@ -41,22 +55,21 @@ const Home: NextPage = () => {
     let tipsList = document.getElementById("tips") as HTMLUListElement;
     let tips = correctWord.tips;
 
-    if (!tips[currentTip +1]) {
+    if (!tips[currentTip + 1]) {
       setPoints(0);
-      nextLevel();  
+      nextLevel();
     } else {
       // Creates a new tip and push it to the list
       let li = tipsList.appendChild(document.createElement("li"));
 
-      li.innerHTML = tips[currentTip +1];
+      li.innerHTML = tips[currentTip + 1];
       li.className = "tip";
 
-      setTip(currentTip +1);
+      setTip(currentTip + 1);
     }
   };
 
   const sendWord = () => {
-
     let brand = correctWord.brand;
 
     // Get the value of the input and turns it to lower case.
@@ -67,29 +80,32 @@ const Home: NextPage = () => {
       input.value = "";
       input.placeholder = "Digite uma marca ou empresa válida!";
 
-      return setTimeout(() => input.placeholder = "", 3000);
-    };
+      return setTimeout(() => (input.placeholder = ""), 3000);
+    }
 
     // If the typed word is correct or not
     if (lower === brand.toLowerCase()) {
       // Very hard thernary, good luck to understand ;)
-      setPoints(currentTip == 0 ? points +3 : currentTip == 1 ? points +2 : points +1);
+      setPoints(
+        currentTip == 0 ? points + 3 : currentTip == 1 ? points + 2 : points + 1
+      );
       nextLevel();
     } else {
       for (let char of lower) {
         // The index of the char in the typed word (lower).
         let index = lower.indexOf(char);
         // Get the list of li elements.
-        let li = Array.from(document.querySelectorAll("li"))
-          .find((i) => i.innerHTML.toLowerCase() == char) as HTMLLIElement;
+        let li = Array.from(document.querySelectorAll("li")).find(
+          (i) => i.innerHTML.toLowerCase() == char
+        ) as HTMLLIElement;
 
         // Change the color of the keyboard buttons
         if (li && brand.includes(char) && brand.indexOf(char) == index) {
           li.style.backgroundColor = "green";
-        } else if (li &&brand.includes(char)) {
+        } else if (li && brand.includes(char)) {
           li.style.backgroundColor = "yellow";
         } else if (li) {
-          li.style.backgroundColor = "gray";
+          li.style.backgroundColor = "red";
         }
       }
 
@@ -108,6 +124,15 @@ const Home: NextPage = () => {
         <title>Naming</title>
       </Head>
       <main>
+        <audio id="sound" src="/click.mp3" />
+        <input
+          title="-"
+          value="Guess the Brand"
+          id="randomBrand"
+          className={styles.randomBrand}
+          type="text"
+          readOnly
+        />
         <section className={styles.interface}>
           <input
             id="word"
@@ -115,9 +140,7 @@ const Home: NextPage = () => {
             type="text"
             maxLength={correctWord.brand.length}
             onKeyDown={(ev) => {
-              if (ev.key == "Enter") {
-                sendWord();
-              }
+              if (ev.key == "Enter") sendWord();
             }}
           />
           <br />
@@ -142,27 +165,42 @@ const Home: NextPage = () => {
                     let input = document.getElementById(
                       "word"
                     ) as HTMLInputElement;
+                    let audio = document.getElementById(
+                      "sound"
+                    ) as HTMLAudioElement;
 
                     if (input.value.length < correctWord.brand.length) {
                       input.value += ev.currentTarget.innerHTML;
+
+                      audio.volume = 0.6;
+                      audio.play();
                     }
-                  }} 
+                  }}
                 >
                   {ch.toUpperCase()}
                 </li>
               );
             })}
+
+            <li
+              onClick={() => {
+                let input = document.getElementById("word") as HTMLInputElement;
+                input.value = "";
+              }}
+            >
+              Limpar
+            </li>
           </ul>
         </section>
       </main>
       <footer className={styles.footer}>
         <b>
           Créditos:&nbsp;
-          <a href="https://github.com/DuckCoder1101">DuckCoder1101</a>. 
+          <a href="https://github.com/DuckCoder1101">DuckCoder1101</a>.
         </b>
       </footer>
     </div>
   );
-}
+};
 
-export default Home
+export default Home;
